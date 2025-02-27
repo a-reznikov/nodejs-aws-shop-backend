c3195d73bb6324721c377536a0dd651c53724e20
a-reznikov <a.reznnikov@gmail.com>
2025-02-27 09:31:57 +0300

    feat: install uuid ti project

--- product-service/lambda/products/products.ts
+++ product-service/lambda/products/products.ts
@@ -4,42 +4,42 @@ export const products: Product[] = [
   {
     description:
       "A novel by George Orwell, depicting a dystopian future society.",
-    id: "1",
+    id: "b2914b76-7b27-4eab-8db0-2ebc21a02d94",
     price: 12,
     title: "1984",
   },
   {
     description:
       "A classic novel by Harper Lee, exploring racial injustice in the American South.",
-    id: "2",
+    id: "8d82e0f4-1cea-495f-b731-e6c406075a4d",
     price: 10,
     title: "To Kill a Mockingbird",
   },
   {
     description:
       "A fantasy novel by J.R.R. Tolkien, the first part of The Lord of the Rings series.",
-    id: "3",
+    id: "6674ea28-0f3f-4a32-8ca5-5a7eb6fe9f21",
     price: 15,
     title: "The Fellowship of the Ring",
   },
   {
     description:
       "A novel by F. Scott Fitzgerald, portraying the Jazz Age and the American Dream.",
-    id: "4",
+    id: "65730bec-8ad6-4e65-9621-f8351bd5826f",
     price: 11,
     title: "The Great Gatsby",
   },
   {
     description:
       "A novel by J.D. Salinger, following the life of a disenchanted teenager.",
-    id: "5",
+    id: "0986f062-c017-4364-9661-da5393a8b30d",
     price: 9,
     title: "The Catcher in the Rye",
   },
   {
     description:
       "A novel by Jane Austen, exploring themes of love, class, and reputation.",
-    id: "6",
+    id: "25167273-e150-4e12-9ddb-8bdb4148ea9f",
     price: 8,
     title: "Pride and Prejudice",
   },
