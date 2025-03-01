a909b68b8d70c9c795673794ddc98110e9e92b52
a-reznikov <a.reznnikov@gmail.com>
2025-03-01 09:22:14 +0300

    feat: rewrite getProductsList

--- product-service/lambda/getProductsList.ts
+++ product-service/lambda/getProductsList.ts
@@ -1,12 +1,33 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, ScanCommand } from "@aws-sdk/lib-dynamodb";
 import { headers } from "./api/constants";
-import { products } from "../db/products/products";
+
+const client = new DynamoDBClient({});
+const documentClient = DynamoDBDocumentClient.from(client);
 
 export const handler = async () => {
   try {
+    const { Items: products } = await documentClient.send(
+      new ScanCommand({ TableName: process.env.DYNAMO_DB_PRODUCTS })
+    );
+    const { Items: stocks } = await documentClient.send(
+      new ScanCommand({ TableName: process.env.DYNAMO_DB_STOCKS })
+    );
+
+    const combinedData = products?.map((product) => ({
+      ...product,
+      count: stocks?.find(({ product_id }) => product_id === product.id)?.count,
+    }));
+
+    console.log(
+      "getProductsList was successfully completed. Result",
+      combinedData
+    );
+
     return {
       statusCode: 200,
       headers,
-      body: JSON.stringify(products),
+      body: JSON.stringify(combinedData),
     };
   } catch (error: any) {
     return {
