f297f758ac89e449c7c7c7f0a073739b6c42fbd7
a-reznikov <a.reznnikov@gmail.com>
2025-04-18 20:40:39 +0300

    feat: update service route logic

--- bff-service/routes/root.js
+++ bff-service/routes/root.js
@@ -10,8 +10,8 @@ export default async function (fastify, opts) {
   })
 
   fastify.all('/:service/*', async function (request, reply) {
-    const { service } = request.params
-    const recipientURL = process.env[service]
+    const { service } = request.params;
+    const recipientURL = process.env[service];
 
     if (!recipientURL) {
       return reply.code(502).send({
@@ -21,28 +21,28 @@ export default async function (fastify, opts) {
       })
     }
 
+    const path = request.url.slice(request.url.indexOf(`/${service}/`) + service.length + 2);
+    const targetUrl = new URL(`${recipientURL}/${path}`);
+
+    Object.entries(request.query).forEach(([key, value]) => {
+      targetUrl.searchParams.append(key, value)
+    });
+
+    console.log('targetUrl', targetUrl);
+
+    const fetchOptions = {
+      method: request.method,
+      headers: { ...request.headers },
+      ...(request.method !== 'GET' && request.body ? { body: request.body } : {})
+    }
+
+    delete fetchOptions.headers.host
+
+    console.log('fetchOptions', JSON.stringify(fetchOptions, null, 2));
+    console.log('targetUrl', JSON.stringify(targetUrl, null, 2));
+
     try {
-      const path = request.url.slice(request.url.indexOf(`/${service}/`) + service.length + 2);
-      const targetUrl = new URL(`${recipientURL}/${path}`);
-
-      Object.entries(request.query).forEach(([key, value]) => {
-        targetUrl.searchParams.append(key, value)
-      })
-
-      console.log('targetUrl', targetUrl);
-
-      const fetchOptions = {
-        method: request.method,
-        headers: { ...request.headers },
-        ...(request.method !== 'GET' && request.body ? { body: request.body } : {})
-      }
-
-      delete fetchOptions.headers.host
-
-      console.log('fetchOptions', fetchOptions);
-      console.log('targetUrl', targetUrl);
-
-      const response = await fetch(targetUrl, fetchOptions)
+      const response = await fetch(targetUrl, fetchOptions);
 
       reply.code(response.status)
 
@@ -56,10 +56,10 @@ export default async function (fastify, opts) {
     } catch (error) {
       fastify.log.error(`Error forwarding request: ${error.message}`)
 
-      return reply.code(502).send({
-        error: 'Bad Gateway',
-        message: 'Error forwarding request',
-        statusCode: 502
+      return reply.code(500).send({
+        error: 'Internal Server Error',
+        message: 'Something went wrong',
+        statusCode: 500
       })
     }
   })
